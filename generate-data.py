e061ea78d87723c9bcc9c30b950d27a578eafb05
Brian Cain <brianccain@gmail.com>
2016-03-20 11:47:27 -0700

    Add method for graphing calories against bodyweight over time

--- generate-data.py
+++ generate-data.py
@@ -25,20 +25,17 @@ def macros_barchart(data):
     protein = go.Bar(
             x = x_axis,
             y = y_protein,
-            name = 'Protein'
-    )
+            name = 'Protein')
 
     carbs = go.Bar(
             x = x_axis,
             y = y_carbs,
-            name = 'Carbohydrates'
-    )
+            name = 'Carbohydrates')
 
     fat = go.Bar(
             x = x_axis,
             y = y_fat,
-            name = 'Fat'
-    )
+            name = 'Fat')
 
     total_data = [protein, carbs, fat]
     layout = go.Layout(barmode='stack')
@@ -62,6 +59,41 @@ def weight_chart(weight):
 
     data = [go.Scatter(x=x_axis, y=y_axis)]
     plotly.offline.plot(data, filename='graphs/weight-series.html')
+
+def weights_vs_cals_chart(data, weight):
+    # https://plot.ly/python/multiple-axes/
+    x_cals = []
+    y_cals = []
+    x_weight = weight.keys()
+    y_weight = weight.values()
+
+    for d in data:
+        x_cals.append(d.date)
+        y_cals.append(d.totals['calories'])
+
+    calories = go.Scatter(
+            x = x_cals,
+            y = y_cals,
+            name = 'Calories')
+
+    weights = go.Scatter(
+            x = x_weight,
+            y = y_weight,
+            name = 'Total weight',
+            yaxis = 'y2')
+
+    total_data = [calories, weights]
+
+    layout = go.Layout(
+            title = 'Weight vs Calories over time',
+            yaxis = dict(title='Calories'),
+            yaxis2 = dict(
+                title = 'Weight',
+                titlefont = dict(color='rgb(148, 103, 189)'),
+                tickfont = dict(color='rgb(148, 103, 189)'),
+                overlaying = 'y',
+                side = 'right'))
+    plotly.offline.plot(total_data, filename='graphs/weight-cals-series.html')
 
 def get_data(mfp_client, start_date):
     d1 = start_date
@@ -91,6 +123,7 @@ def get_login():
 def generate_charts(data, body_weights):
     macros_barchart(data)
     total_calories_chart(data)
+    weights_vs_cals_chart(data, body_weights)
     #weight_chart(body_weights)
 
 def main():
@@ -98,7 +131,7 @@ def main():
     #start_date = date(2013, 8, 31) # original start date on MFP
     #start_date = date(2013, 7, 28) # first weight record, no meals
 
-    start_date = date(2016, 3, 13)
+    start_date = date(2016, 1, 1)
 
     client = myfitnesspal.Client(login_info['username'], login_info['password'])
 
