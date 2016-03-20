f0817e0264da7e5c5d08d18c09d89983959b89be
Brian Cain <brianccain@gmail.com>
2016-03-20 14:51:04 -0700

    Add macros pie chart and extra debug logging because mfp api is bad

--- generate-data.py
+++ generate-data.py
@@ -42,6 +42,29 @@ def macros_barchart(data):
     figure = go.Figure(data=total_data, layout=layout)
     plotly.offline.plot(figure, filename='graphs/stacked-bar-macros.html')
 
+def macros_piechart(data):
+    total_protein = 0
+    total_carbs = 0
+    total_fat = 0
+
+    for d in data:
+        total_protein += d.totals['protein']
+        total_carbs += d.totals['carbohydrates']
+        total_fat += d.totals['fat']
+
+    avg_protein = total_protein / len(data)
+    avg_carbs = total_carbs / len(data)
+    avg_fat = total_fat / len(data)
+
+    figure = {
+        'data': [{'labels': ['Protein', 'Carbohydrates', 'Fat'],
+                  'values': [avg_protein, avg_carbs, avg_fat],
+                  'type': 'pie'}],
+        'layout': {'title': 'Average macro nutrients'}
+    }
+
+    plotly.offline.plot(figure, filename='graphs/avg-macro-pie.html')
+
 def total_calories_chart(data):
     x_axis = []
     y_axis = []
@@ -99,13 +122,11 @@ def get_data(mfp_client, start_date):
     d1 = start_date
     d2 = date.today()
     delta = d2 - d1
-
     data = []
 
-    # build up a list of dates and meals from beginning to today
-    # ...this is gonna take a while...
-    print "Retrieving data from myfitnesspal....this will take a while..."
+    print "Retrieving data from myfitnesspal....this will take a while...\n"
     for i in range(delta.days + 1):
+        print "Getting date " + str(d1 + timedelta(days=i)) + " . . ."
         mfp_data = mfp_client.get_date(d1 + timedelta(days=i))
         data.append(mfp_data)
 
@@ -121,22 +142,25 @@ def get_login():
             exit(1)
 
 def generate_charts(data, body_weights):
+    print "Generating charts . . ."
     macros_barchart(data)
     total_calories_chart(data)
     weights_vs_cals_chart(data, body_weights)
-    #weight_chart(body_weights)
+    weight_chart(body_weights)
+    macros_piechart(data)
 
 def main():
     login_info = get_login()
     #start_date = date(2013, 8, 31) # original start date on MFP
     #start_date = date(2013, 7, 28) # first weight record, no meals
 
-    start_date = date(2016, 1, 1)
+    start_date = date(2015, 3, 15)
 
+    print "Logging in as " + login_info['username'] + "..."
     client = myfitnesspal.Client(login_info['username'], login_info['password'])
+    print "Done!"
 
     body_weights = client.get_measurements('Weight', start_date)
-
     data = get_data(client, start_date)
 
     generate_charts(data, body_weights)
